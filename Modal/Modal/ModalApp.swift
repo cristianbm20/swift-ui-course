c4ee3420cdd828f52d415c125c0906fc72994582
Cristian Bermudez Martinez <cristianbm2095@gmail.com>
2024-02-27 17:56:07 +0100

    Stop tracking PokedexProject

---
+++ Modal/Modal/ModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalApp.swift
+//  Modal
+//
+//  Created by Cristian Bermudez Martinez on 27/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
