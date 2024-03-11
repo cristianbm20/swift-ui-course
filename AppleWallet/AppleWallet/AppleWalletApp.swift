823ee3260b0f4efadc6d861b5c659369b5674f63
Cristian Bermudez Martinez <cristianbm2095@gmail.com>
2024-03-11 20:00:18 +0100

    Day 34: Start day 18 of the 50 Days of iOS 17 course

---
+++ AppleWallet/AppleWallet/AppleWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWalletApp.swift
+//  AppleWallet
+//
+//  Created by Cristian Bermudez Martinez on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
