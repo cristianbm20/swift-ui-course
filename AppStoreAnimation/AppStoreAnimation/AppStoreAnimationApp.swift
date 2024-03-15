ee130a04cb9d1ba7091fcd8cbdd9b25814eb3f14
Cristian Bermudez Martinez <cristianbm2095@gmail.com>
2024-03-15 10:27:23 +0100

    Day 38: Start day 22 of the 50 Days of iOS 17 course

---
+++ AppStoreAnimation/AppStoreAnimation/AppStoreAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreAnimationApp.swift
+//  AppStoreAnimation
+//
+//  Created by Cristian Bermudez Martinez on 15/3/24.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreAnimationApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
