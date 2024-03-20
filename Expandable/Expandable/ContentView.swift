ee20ec9577d8bc3787334e865e40a1b899f4bfbe
Cristian Bermudez Martinez <cristianbm2095@gmail.com>
2024-03-20 16:31:24 +0100

    Day 39: Start day 24 of the 50 Days of iOS 17 course

---
+++ Expandable/Expandable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Expandable
+//
+//  Created by Cristian Bermudez Martinez on 20/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ Expandable/Expandable/ExpandableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandableApp.swift
+//  Expandable
+//
+//  Created by Cristian Bermudez Martinez on 20/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpandableApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
