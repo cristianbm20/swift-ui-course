9d597a1285032f4cdedbcc47eec6f122e21183bf
Cristian Bermudez Martinez <cristianbm2095@gmail.com>
2024-03-26 11:01:52 +0100

    Day 42: Start day 27 of the 50 Days of iOS 17 course

---
+++ MoreTexts/MoreTexts/MoreTextsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoreTextsApp.swift
+//  MoreTexts
+//
+//  Created by Cristian Bermudez Martinez on 26/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MoreTextsApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
