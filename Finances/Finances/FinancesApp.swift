d3781aef374e5d15cd81a0b84a7021707a38e751
Cristian Bermudez Martinez <cristianbm2095@gmail.com>
2024-03-27 09:52:04 +0100

    Day 43: Start day 30 of the 50 Days of iOS 17 course

---
+++ Finances/Finances/FinancesApp.swift
@@ -0,0 +1,32 @@
+//
+//  FinancesApp.swift
+//  Finances
+//
+//  Created by Cristian Bermudez Martinez on 27/3/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FinancesApp: App {
+  var sharedModelContainer: ModelContainer = {
+    let schema = Schema([
+      Item.self,
+    ])
+    let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+    
+    do {
+      return try ModelContainer(for: schema, configurations: [modelConfiguration])
+    } catch {
+      fatalError("Could not create ModelContainer: \(error)")
+    }
+  }()
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+    .modelContainer(sharedModelContainer)
+  }
+}
